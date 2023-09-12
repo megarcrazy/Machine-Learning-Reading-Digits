dbcd15931f58f1361c2e8be7ba56e7fb49a0033d
megarcrazy <strangernotjoke@hotmail.com>
2023-09-12 20:17:18 +1000

    Refactor and comments

--- src/model.py
+++ src/model.py
@@ -1,25 +1,37 @@
-from src.dataTransformer import DataTransformer
+from typing import Tuple, List
+import sklearn
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 from src.scenes.graph import Graph
+from src.dataTransformer import DataTransformer
 
 
 class Model:
-    def __init__(self, data=None, playing=False):
-        # Uses data from local database to produce a model
+    """
+    Machine learning model to fit on input data for training and
+    non-training mode.
+    """
+
+    def __init__(
+        self, data: Tuple[List[int], List[int]] = None, playing: bool = False
+    ):
         self._x, self._y = data
         self._model = SVC
         self._clf = self._model(gamma=0.1, C=10)
         self._clf = CalibratedClassifierCV(self._clf)
 
     def fit_model(self):
+        """Fit model on training data."""
         self._clf.fit(self._x, self._y)
 
-    # Calculates the predicted accuracy of the model by using the stored data
-    # and splitting the data into training and testing data
     def evaluate_accuracy(self):
+        """
+        Calculates the predicted accuracy of the model by using the stored data
+        and splitting the data into training and testing data.
+        """
         intervals = 30
         amount_list = []
         accuracy_list = []
@@ -33,7 +45,10 @@ class Model:
         Graph.draw_graph(amount_list, accuracy_list)
 
     @staticmethod
-    def _get_accuracy(x, y, model):
+    def _get_accuracy(
+        x: List[int], y: List[int], model: sklearn.base.ClassifierMixin
+    ):
+        """Get accuracy of training data."""
         x_train, x_test, y_train, y_test = train_test_split(
             x, y, test_size=0.2
         )
@@ -43,26 +58,18 @@ class Model:
         accuracy = accuracy_score(y_test, y_prediction)
         return accuracy
 
-    # Get computer predicted number and probability
-    def predict(self, fit_data):
+    def predict(self, fit_data: List[List[int]]):
+        """Get computer predicted number and probability of input data."""
         fit_data = [DataTransformer.centre_canvas(fit_data[0])]
         number = int(self._clf.predict(fit_data))
         probability = max(self._clf.predict_proba(fit_data)[0])
         return number, probability
 
-    # Find optimal Gamma and C for the SVC model
     def _find_optimal_parameters(self):
-        # Code from "Introduction toMachineLearning with Python by Andreas C.
-        # Müller & Sarah Guido" page 306
-        from sklearn.model_selection import GridSearchCV
-
+        """Find optimal Gamma and C for the SVC model."""
         param_grid = {
             "C": [0.001, 0.01, 0.1, 1, 10, 100],
             "gamma": [0.001, 0.01, 0.1, 1, 10, 100],
         }
         grid = GridSearchCV(SVC(), param_grid=param_grid, cv=5)
         grid.fit(self._x, self._y)
-        print(
-            "Best cross-validation accuracy: {:.2f}".format(grid.best_score_)
-        )
-        print("Best parameters: ", grid.best_params_)
--- src/window.py
+++ src/window.py
@@ -4,6 +4,8 @@ import src.constants as c
 
 
 class WindowApplication:
+    """Application class to run pygame application."""
+
     def __init__(self):
         pygame.init()
         self._screen = pygame.display.set_mode(
@@ -13,16 +15,23 @@ class WindowApplication:
         pygame.display.set_caption("Machine Learning Reading Digits")
 
     def run(self):
+        """
+        Run constant loop to render application and listen to user inputs.
+        """
         clock = pygame.time.Clock()
         state = True
         while state:
+            # Check if exit button is clicked
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     state = False
 
+            # Clear pygame application image each loop
             self._screen.fill(c.WHITE)
+
+            # Update backend and frontend of application
             self._scene_manager.update()
             self._scene_manager.render()
 
-            pygame.display.flip()
+            pygame.display.flip()  # Draw image on application
             clock.tick(c.FPS)
